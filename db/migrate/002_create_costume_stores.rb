ac37c63184f910415b2ca8eecb2673f85ef8903d
Chloe Gell <gellert.chloe@gmail.com>
2020-03-06 17:53:20 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -10,6 +10,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       c.boolean :still_in_business
       c.datetime :opening_time
       c.datetime :closing_time
+      c.timestamps 
     end
   end
 end
