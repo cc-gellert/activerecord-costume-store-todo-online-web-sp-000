6456427b7c46172dfc9f9bee895b13e341efdc1e
Chloe Gell <gellert.chloe@gmail.com>
2020-03-06 17:52:53 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,6 +3,7 @@
 class CreateCostumeStores < ActiveRecord::Migration[5.1]
   def change
     create_table :costume_stores do |c|
+      c.string :name 
       c.string :location
       c.integer :costume_inventory
       c.integer :num_of_employees
