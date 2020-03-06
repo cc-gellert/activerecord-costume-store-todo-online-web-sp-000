02d0e46d9486c3da04afde00165a26d4373a4d01
Chloe Gell <gellert.chloe@gmail.com>
2020-03-06 17:54:37 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -10,7 +10,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       h.boolean :family_friendly
       h.datetime :opening_date
       h.datetime :closing_date
-      h.string :description
+      h.text :description
     end
   end
 end
