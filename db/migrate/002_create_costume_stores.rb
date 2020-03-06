cf46503c1dad859b405ff84320dcfdf734715a64
Chloe Gell <gellert.chloe@gmail.com>
2020-03-06 17:52:34 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,13 +2,13 @@
 
 class CreateCostumeStores < ActiveRecord::Migration[5.1]
   def change 
-    create_table :costumestores do |c|
+    create_table :costume_stores do |c|
       c.string :location 
       c.integer :costume_inventory
       c.integer :num_of_employees 
       c.boolean :still_in_business 
-      c.string :opening_time 
-      c.string :closing_time 
+      c.datetime :opening_time 
+      c.datetime :closing_time 
     end 
   end 
 end 
